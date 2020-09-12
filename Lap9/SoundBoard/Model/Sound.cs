9e6fd5ee1f3fc7308e568fc82e46ccaf24f57d52
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-09-13 00:36:45 +0700

    lap9

---
+++ Lap9/SoundBoard/Model/Sound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Model
+{
+    public class Sound
+    {
+        public string Name { get; set; }
+        public SoudCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+        public Sound(string name, SoudCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = String.Format("/Assets/Audio/{0}/{1}.wav", category, name);
+            ImageFile = String.Format("/Assets/Images/{0}/{1}.png", category, name);
+
+        }
+    }
+    public enum SoudCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+    }
+    
+}
---
+++ Lap9/SoundBoard/Model/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoundBoard.Model;
+
+namespace SoundBoard.Model
+{
+    class SoundManager
+    {
+      
+        public static void GetSoundByCategory(ObservableCollection<Sound> sounds, SoudCategory soudCategory)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == soudCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+            allSounds.ForEach(p => sounds.Add(p));
+        }
+
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoudCategory.Animals));
+            sounds.Add(new Sound("Cat", SoudCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoudCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoudCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoudCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoudCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoudCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoudCategory.Warnings));
+
+            return sounds;
+        }
+        public static void GetSoundsByName(ObservableCollection<Sound> sounds,string name)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Name == name).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+    }
+}
