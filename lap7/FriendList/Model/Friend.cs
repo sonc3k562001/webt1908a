de88bb743b773f14714a3f8c25c1b7176969cb10
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-09-12 15:32:10 +0700

    lap7

---
+++ lap7/FriendList/Model/Friend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendList.Model
+{
+    class Friend
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Interests { get; set; }
+        public string Address { get; set; }
+        public string Numberphone { get; set; }
+        public string Birthday { get; set; }
+        public string Avatar { get; set; }
+
+    }
+}
---
+++ lap7/FriendList/Model/ListFriend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendList.Model
+{
+    class ListFriend
+    {
+        public static List<Friend> GetFriends()
+        {
+            var friends = new List<Friend>();
+
+            friends.Add(new Friend { Name = "Le Pham Minh Ngoc", Age = 18, Birthday = "06-11-2001", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/male-01.png" });
+            friends.Add(new Friend { Name = "Nguyen Viet Hoang", Age = 18, Birthday = "06-11-2001", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/male-02.png" });
+            friends.Add(new Friend { Name = "Pham Thanh Ha", Age = 18, Birthday = "06-11-2001", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/male-03.png" });
+            friends.Add(new Friend { Name = "Le Hai Anh", Age = 20, Birthday = "06-11-1999", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/female-01.png" });
+            friends.Add(new Friend { Name = "Le Huyen Nhi", Age = 18, Birthday = "06-11-2001", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/female-02.png" });
+            friends.Add(new Friend { Name = "Nguyen Thuy Trang", Age = 18, Birthday = "06-11-2001", Address="Moc nam - Duy Tien - Ha Nam",Numberphone="0189131931",Interests="Anime, Fime,Black Coffee,game,...",Avatar="Assets/female-03.png" });
+
+            return friends;
+        }
+    }
+}
