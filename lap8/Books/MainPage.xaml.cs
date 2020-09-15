3eac9e87336a2537c9d8d01d6d7edeefef89e51f
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-09-15 16:34:27 +0700

    lap8

---
+++ lap8/Books/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Books.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Books
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<BooksItem> BooksItems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BooksItems = new ObservableCollection<BooksItem>();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Coder.IsSelected)
+            {
+                BooksManager.GetBooks("Coder", BooksItems);
+                TitleTextBlock.Text = "Coder";
+            }
+            else if(Developer.IsSelected)
+            {
+                BooksManager.GetBooks("Developer", BooksItems);
+                TitleTextBlock.Text = "Developer";
+            }
+        }
+
+        private void CoderButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Coder.IsSelected = true;
+        }
+    }
+}
---
+++ lap8/Books/Model/BooksItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Model
+{
+    public class BooksItem
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string content { get; set; }
+    }
+    public class BooksManager
+    {
+        public static void GetBooks(string Title,ObservableCollection<BooksItem> booksItems)
+        {
+            var allItems = getBooksItem();
+
+            var filteredBooksItems = allItems.Where(p => p.title == Title).ToList();
+
+            booksItems.Clear();
+
+            filteredBooksItems.ForEach(p => booksItems.Add(p));
+        }
+        private static List<BooksItem> getBooksItem()
+        {
+            var books = new List<BooksItem>();
+
+            books.Add(new BooksItem() { id = 1, title = "Coder", image = "Assets/1.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 2, title = "Coder", image = "Assets/2.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 3, title = "Coder", image = "Assets/3.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 4, title = "Coder", image = "Assets/4.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 5, title = "Coder", image = "Assets/5.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 6, title = "Coder", image = "Assets/7.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 8, title = "Coder", image = "Assets/8.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 9, title = "Coder", image = "Assets/9.png", content = "This is books,ahihi" });
+
+            books.Add(new BooksItem() { id = 10, title = "Developer", image = "Assets/10.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 11, title = "Developer", image = "Assets/11.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 12, title = "Developer", image = "Assets/12.png", content = "This is books,ahihi" });
+            books.Add(new BooksItem() { id = 13, title = "Developer", image = "Assets/13.png", content = "This is books,ahihi" });
+            return books;
+        }
+    }
+}
