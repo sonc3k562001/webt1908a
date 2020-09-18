61ca27096ebbcd71637f299c9f12e111c66c66f4
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-09-18 16:31:12 +0700

    ok

---
+++ Practice/Practice/Model/Img.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Model
+{
+    public class Img
+    {
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
