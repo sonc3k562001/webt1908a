31ec775912bee752c48754d1e0a2ac35c24426f6
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-10-03 23:52:55 +0700

    lap4

---
+++ lap4/Book/Book/Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Book.Models;
+using Microsoft.AspNetCore.Rewrite.Internal.UrlActions;
+
+namespace Book.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+ 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(
+            [Bind("ID,Title,ReleaseDate,Cost")] BookModel bookModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectAction("Index");
+            }
+            return View(bookModel);
+        }
+
+        private IActionResult RedirectAction(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IActionResult View(BookModel bookModel)
+        {
+            throw new NotImplementedException();
+        }
+    }  
+}
